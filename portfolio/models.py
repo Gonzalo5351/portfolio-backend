05a6641391c66aa78fd115709c7eea7191775fa7
Gonzalo <gonzalovelazco32@gmail.com>
2024-10-10 17:09:31 -0300

    Implemented authentication system, added project management (edit, delete), created templates (login, register, contact), updated models and migrations

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Proyecto(models.Model):
-    titulo = models.CharField(max_length=200)  # Título del proyecto
-    descripcion = models.TextField()  # Descripción del proyecto
-    tecnologias = models.CharField(max_length=200)  # Tecnologías usadas
-    enlace = models.URLField(max_length=200)  # Enlace al repositorio o demo
-    fecha = models.DateField()  # Fecha de creación del proyecto
+    title = models.CharField(max_length=200)  # Título del proyecto
+    description = models.TextField()  # Descripción del proyecto
+    technologies = models.CharField(max_length=200)  # Tecnologías usadas
+    link = models.URLField(max_length=200)  # Enlace al repositorio o demo
+    date = models.DateField()  # Fecha de creación del proyecto
 
     def __str__(self):
         return self.titulo  # Lo que se mostrará cuando veas un Proyecto en texto
