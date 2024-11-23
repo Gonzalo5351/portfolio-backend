7fad3bd8825dbb9bbbfe64464fba9be4648a023b
Gonzalo <gonzalovelazco32@gmail.com>
2024-11-23 15:04:06 -0300

    Adding a temporaly endponit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,16 +14,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.http import JsonResponse
+from django.conf import settings
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Initial page
-    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    path('contact/', views.contact, name='contact'),
-    path('auth/', include('portfolio.urls_auth')),  # Authentication URL
-    path('manage-projects/', views.manage_projects, name='manage_projects'),
-    path('add-project/', views.add_project, name='add_project'),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),  # Initial page
+    path("projects/<int:project_id>/", views.project_detail, name="project_detail"),
+    path("contact/", views.contact, name="contact"),
+    path("auth/", include("portfolio.urls_auth")),  # Authentication URL
+    path("manage-projects/", views.manage_projects, name="manage_projects"),
+    path("add-project/", views.add_project, name="add_project"),
+]
+
+
+def staticfiles_debug_view(request):
+    # Ruta del directorio donde se recopilan los archivos estáticos
+    staticfiles_dir = settings.STATIC_ROOT
+    if os.path.exists(staticfiles_dir):
+        files = []
+        for root, dirs, filenames in os.walk(staticfiles_dir):
+            for filename in filenames:
+                relative_path = os.path.relpath(
+                    os.path.join(root, filename), staticfiles_dir
+                )
+                files.append(relative_path)
+        return JsonResponse({"files": files})
+    return JsonResponse({"error": "No static files found."}, status=404)
+
+
+urlpatterns += [
+    path("staticfiles-debug/", staticfiles_debug_view),
 ]
